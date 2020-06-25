a0df7353886ad67232757c3f497fd0b9f69e2272
Edward Upton <edward@littledata.io>
2020-06-25 06:57:45 +0100

    Timeout for ad blockers

--- client/buyNow/getGoogleClientId.js
+++ client/buyNow/getGoogleClientId.js
@@ -1,17 +1,24 @@
 export const getGoogleClientId = () => {
 	return new Promise(resolve => {
-		// unfortunately gtag only makes 'ga' function available after the library loads
-		// so we have to stub it first
+		// gtag only makes 'ga' function available after the library loads
+		// so we have to stub it if undefined here
 		window.ga =
 			window.ga ||
 			function() {
 				;(window.ga.q = window.ga.q || []).push(arguments) //eslint-disable-line
 			}
 		window.ga.l = +new Date()
+
+		const fallback = window.setTimeout(function() {
+			//after 4 seconds, assume the script is blocked
+			resolve('')
+		}, 4000)
 		window.ga(function() {
 			// this function is called after GA library initializes
+			window.clearTimeout(fallback)
 			const tracker = window.ga.getAll()[0]
 			const clientId = tracker && tracker.get('clientId')
+
 			return resolve(clientId)
 		})
 	})
--- client/buyNow/getSegmentAnonymousId.js
+++ client/buyNow/getSegmentAnonymousId.js
@@ -1,6 +1,11 @@
 export const getSegmentAnonymousId = () => {
 	return new Promise(resolve => {
+		const fallback = window.setTimeout(function() {
+			//after 4 seconds, assume the script is blocked
+			resolve('')
+		}, 4000)
 		window.analytics.ready(function() {
+			window.clearTimeout(fallback)
 			resolve(window.analytics.user().anonymousId())
 		})
 	})
